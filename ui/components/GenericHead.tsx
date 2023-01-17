c56f47334229f2a9333c3c48408d529695a62dd5
srslafazan <srslafazan@gmail.com>
2023-01-17 14:59:37 -0500

    Updated favicon

--- ui/components/GenericHead.tsx
+++ ui/components/GenericHead.tsx
@@ -5,6 +5,9 @@ export const GenericHead = () => (
   <Head>
     <title>{pkg.company.name}</title>
     <meta name="description" content={pkg.description} />
-    <link rel="icon" href="/favicon.ico" />
+    <link
+      rel="icon"
+      href="https://www.withhugo.com/assets/images/favicon/favicon.ico?v=2"
+    />
   </Head>
 );
